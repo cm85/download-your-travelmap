0607bea6a47ae23e51595bb791ff0f0061cb545d
Haller, Christian <haller.christian@guj.de>
2016-06-22 09:51:19 +0200

    normalise

--- gulp/tasks/prod/styles.js
+++ gulp/tasks/prod/styles.js
@@ -1,25 +1,31 @@
-/*global require*/
+/*global require, __dirname */
 (function (require) {
     'use strict';
     var gulp = require('gulp'),
+        path = require('path'),
         config = require('../../config'),
         rename = require('gulp-rename'),
         csso = require('gulp-csso'),
         please = require('gulp-pleeease'),
         minifycss = require('gulp-minify-css'),
         concat = require('gulp-concat'),
-        //sassLint = require('gulp-sass-lint'),
-        sass = require('gulp-sass');
+    //sassLint = require('gulp-sass-lint'),
+        sass = require('gulp-sass'),
+        SASS_INCLUDE_PATHS = [
+            path.join(__dirname, '/node_modules/normalize-css')
+        ];
 
     gulp.task('styles', ['svg'], function () {
         return gulp.src('./app/styles/main.scss')
             // 1 . SASS
             /*.pipe(sassLint({
-                'config': '.scss-lint.yml'
-            }))
-            .pipe(sassLint.format())
-            //.pipe(sassLint.failOnError())*/
-            .pipe(sass())
+             'config': '.scss-lint.yml'
+             }))
+             .pipe(sassLint.format())
+             //.pipe(sassLint.failOnError())*/
+            .pipe(sass({includePaths: SASS_INCLUDE_PATHS
+                }
+                ))
             // 2. autoprefixing
             .pipe(please(config.please))
 
