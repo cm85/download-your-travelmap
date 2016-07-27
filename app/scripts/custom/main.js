2e8ef961938e7bb22d12e726597d9a0edcedfc82
Christian Haller <hallo@christianhaller.com>
2016-07-28 01:48:00 +0200

    anim

--- app/scripts/custom/main.js
+++ app/scripts/custom/main.js
@@ -36,39 +36,79 @@ datamap.addPlugin('pins', function ( layer, data ) {
         .selectAll(className)
         .data(data, JSON.stringify);
 
-    bubbles
-        .enter()
-        .append('image')
-        .attr('xlink:href', 'http://a.tiles.mapbox.com/v3/marker/pin-m+7e7e7e@2x.png')
-        .attr('class', className) //remember to set the class name
-        .attr('height', 40)
-        .attr('width', 40)
-        .attr('x', function (datum) {
-            return self.latLngToXY(datum.lat, datum.lng)[0]-20;
-        })
-        .attr('y', function (datum) {
-            return self.latLngToXY(datum.lat, datum.lng)[1]-20;
-        })
-        .attr('r', 10);
+
+    //SVG filter for the gooey effect
+    //Code taken from http://tympanus.net/codrops/2015/03/10/creative-gooey-effects/
+    var defs = layer.append("defs");
+    var filter = defs.append("filter").attr("id","gooeyCodeFilter");
+    filter.append("feGaussianBlur")
+        .attr("in","SourceGraphic")
+        .attr("stdDeviation","10")
+        //to fix safari: http://stackoverflow.com/questions/24295043/svg-gaussian-blur-in-safari-unexpectedly-lightens-image
+        .attr("color-interpolation-filters","sRGB")
+        .attr("result","blur");
+    filter.append("feColorMatrix")
+        .attr("class", "blurValues")
+        .attr("in","blur")
+        .attr("mode","matrix")
+        .attr("values","1 0 0 0 0  0 1 0 0 0  0 0 1 0 0  0 0 0 18 -5")
+        .attr("result","gooey");
+    filter.append("feBlend")
+        .attr("in","SourceGraphic")
+        .attr("in2","gooey")
+        .attr("operator","atop");
+
+    layer.selectAll(".xcities")
+        .data(data)
+        .enter().append("circle")
+        .attr("class", "cities")
+        .attr('cx', 10)
+        .attr('cy', 10)
+        .style("opacity", 1);
+
+    function placeCities () {
+
+        //Stop the force layout (in case you move backward)
+       // force.stop();
+
+
+        //Put the cities in their geo location
+        d3.selectAll(".cities")
+            .transition("move").duration(1000)
+            .delay(function(d,i) { return i*20; })
+            .attr("r", function(d) {
+                return 5;
+            })
+            .attr('cx', function (datum) {
+                return self.latLngToXY(datum.lat, datum.lng)[0];
+            })
+            .attr('cy', function (datum) {
+                return self.latLngToXY(datum.lat, datum.lng)[1];
+            })
+
+        //Around the end of the transition above make the circles see-through a bit
+        d3.selectAll(".cities")
+            .transition("dim").duration(2000).delay(4000)
+            .style("opacity", 0.8);
+
+        //"Remove" gooey filter from cities during the transition
+        //So at the end they do not appear to melt together anymore
+        d3.selectAll(".blurValues")
+            .transition().duration(4000)
+            .attrTween("values", function() {
+                return d3.interpolateString("1 0 0 0 0  0 1 0 0 0  0 0 1 0 0  0 0 0 18 -5",
+                    "1 0 0 0 0  0 1 0 0 0  0 0 1 0 0  0 0 0 6 -5");
+            });
+
+    }
+    console.log('place');
+    placeCities();
+
 
 });
 datamap.pins(data.data.places);
 
-datamap.bubbles(data.data.places.map(function (el) {
-    // console.log()
-    'use strict';
-    return {
-        'name': el.name,
-        //   fillKey: el.been,
-        'radius': 4,
 
-        'latitude': el.lat,
-        'longitude': el.lng
-
-    };
-
-
-}));
 
 $(document).ready(function () {
     'use strict';
